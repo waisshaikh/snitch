8b760c8d4bfeb28e46b77333500e63c4c566304f
waisshaikh <waisshaikh394@gmail.com>
2026-07-15 17:33:31 +0530

    fix: add error handling for missing MONGODB_URI in environment variables

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+if(!process.env.MONGODB_URI){
+    throw new Error("MONGODB_URI is not define in envirmentoal variable")
+}
 
 export const config = {
     MONGODB_URI:process.env.MONGODB_URI
