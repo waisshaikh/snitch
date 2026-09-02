e692eccff9ce109fe546dd4341d1572a082d3188
waisshaikh <waisshaikh394@gmail.com>
2026-09-02 23:06:57 +0530

    feat: update Home component layout and styling for improved presentation

--- frontend/src/features/home/Pages/Home.jsx
+++ frontend/src/features/home/Pages/Home.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-yellow-400">
+        hello from snitch
+      </h1>
     </div>
   );
 }
