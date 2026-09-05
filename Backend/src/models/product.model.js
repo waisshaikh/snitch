f473e2f847d6d24b8e2e007f1f4a326e88950896
waisshaikh <waisshaikh394@gmail.com>
2026-09-05 18:08:23 +0530

    feat: add variants field to ProductSchema with stock, attributes, and price details

--- Backend/src/models/product.model.js
+++ Backend/src/models/product.model.js
@@ -31,12 +31,43 @@ const ProductSchema = new mongoose.Schema({
             type: String,
             required: true,
         },
-        
+    ],
 
-    ]
+    variants:[{
+        images:[
+            {
+                url:{
+                    type: String,
+                    required: true
+                }
+            }
+        ],
+
+        stock: {
+            type: Number,
+            default: 0
+        },
+        attributes:{
+            type:Map,
+            of:String
+        },
+        price:{
+            amount:{
+                type:Number,
+                required:true
+            },
+            currency:{
+                type:String,
+                enum:["USD","EUR", "GBP","JPY","INR"],
+                default: "INR"
+            }
+        }
+    }],
 
 
-})
+
+
+},{timestamps:true })
 
 const productModel = mongoose.model("product", ProductSchema)
 
